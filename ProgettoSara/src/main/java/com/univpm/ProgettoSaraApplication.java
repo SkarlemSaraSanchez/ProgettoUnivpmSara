0bc9fe651c0c5342f48583db16c6577acc8e616f
SkarlemSaraSanchez <95294283+SkarlemSaraSanchez@users.noreply.github.com>
2022-01-17 12:00:41 +0100

    Add files via upload

---
+++ ProgettoSara/src/main/java/com/univpm/ProgettoSaraApplication.java
@@ -0,0 +1,13 @@
+package com.univpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSaraApplication.class, args);
+	}
+
+}
