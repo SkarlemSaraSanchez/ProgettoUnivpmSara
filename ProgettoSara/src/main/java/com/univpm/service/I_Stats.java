b359f68f19fc5d4d519f4b56e2c28fcbbbd8ff22
Skarlem Sara Sanchez <hog76973@gmail.com>
2022-01-17 13:13:39 +0100

    aggiunta dichiarazione metodo nell'interfaccia che gestisce il filtro

--- ProgettoSara/src/main/java/com/univpm/service/I_Stats.java
+++ ProgettoSara/src/main/java/com/univpm/service/I_Stats.java
@@ -1,6 +1,13 @@
 package com.univpm.service;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
+import com.univpm.exception.WrongPeriodException;
+
 public interface I_Stats {
 
-	
+	//GESTISCE IL FILTRO
+	public JSONObject gestoreFiltro(String countryCode, String stateCode, String nameClass, String start, String end) 
+			throws ParseException, WrongPeriodException;
 }
