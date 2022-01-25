c51af7b2087db42e89b5026f40c7099ca8367ccd
Skarlem Sara Sanchez <hog76973@gmail.com>
2022-01-25 15:56:09 +0100

    modifiche service

--- ProgettoSara/src/main/java/com/univpm/service/I_Stats.java
+++ ProgettoSara/src/main/java/com/univpm/service/I_Stats.java
@@ -4,10 +4,19 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
 import com.univpm.exception.WrongPeriodException;
+import com.univpm.model.Periodo;
 
 public interface I_Stats {
 
 	//GESTISCE IL FILTRO
 	public JSONObject gestoreFiltro(String countryCode, String stateCode, String nameClass, String start, String end) 
 			throws ParseException, WrongPeriodException;
+
+	public JSONObject getStats(String countryCode, String stateCode) throws ParseException;
+
+	public JSONObject getStats(String countryCode, String stateCode, String nameClass) throws ParseException;
+
+	public JSONObject getStats(String countryCode, String stateCode, Periodo periodo) throws ParseException;
+
+	public JSONObject getStats(String countryCode, String stateCode, String nameClass, Periodo periodo) throws ParseException;
 }
--- ProgettoSara/src/main/java/com/univpm/service/StatsImpl.java
+++ ProgettoSara/src/main/java/com/univpm/service/StatsImpl.java
@@ -70,8 +70,34 @@ public class StatsImpl implements I_Stats {
 	@Override
 	public JSONObject gestoreFiltro(String countryCode, String stateCode, String nameClass, String start, String end)
 			throws ParseException, WrongPeriodException {
+		JSONObject response = new JSONObject();
 		
-		return null;
+		//COSTRUISCO L'OGGETTO PERIODO
+		Periodo periodo = null;
+		if(start != null && end != null) {
+			String[] arr_start = start.split("-");
+			String[] arr_end = end.split("-");
+			periodo = new Periodo(Integer.parseInt(arr_start[0]), Integer.parseInt(arr_start[1]), Integer.parseInt(arr_start[2]), Integer.parseInt(arr_end[0]), Integer.parseInt(arr_end[1]), Integer.parseInt(arr_end[2]));
+		}
+		
+		if(nameClass != null) {
+			//FILTRO CON NAMECLASS E PERIODO
+			if(periodo != null) {
+				response = this.getStats(countryCode,stateCode,nameClass,periodo);
+			}
+			else {
+				response = this.getStats(countryCode,stateCode,nameClass);
+			}
+		}
+		else {
+			if(periodo != null) {
+				response = this.getStats(countryCode,stateCode,periodo);
+			}
+			else {
+				response = this.getStats(countryCode,stateCode);
+			}
+		}
+		return response;
 	}
 
 	/***************************************************************************************************************************/
@@ -172,6 +198,607 @@ public class StatsImpl implements I_Stats {
 		response.put("MIN,MAX,MEDIA", stats_minmaxmed);
 		response.put("TOT EVENTI", tot);
 		response.put("Stats Mensili", arr);
+		return response;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public JSONObject getStats(String countryCode, String stateCode) throws ParseException {
+		JSONObject response = new JSONObject();
+		JSONObject obj = new JSONObject();
+		JSONArray arr= new JSONArray();
+		String[] states = stateCode.split("-");
+		
+		//PER LE STATISTICHE
+		long tot = 0;
+		long min = 0;
+		int indice_min = 0;
+		long max = 0;
+		int indice_max = 0;
+		
+		//VALIDAZIONE PARAMETRO COUNTRYCODE
+		boolean valCountryCode = this.validazione("countryCode", countryCode);
+		if(!valCountryCode) {
+			obj.put("Country", countryCode);
+			obj.put("Errore", "countryCode non valido");
+			obj.put("Messaggio", "Pattern di chiamata compromesso");
+			response = obj;
+			return response;
+		}
+		obj.put("Country", countryCode);
+		
+		//SINGOLO STATE
+		if(states.length == 1) {
+			//VALIDAZIONE PARAMETRO STATECODE
+			boolean valStateCode = this.validazione("stateCode", stateCode);
+			if (!valStateCode) {
+				obj.put("State", stateCode);
+				obj.put("Errore", "stateCode non valido");
+				obj.put("Messaggio", "Pattern di chiamata compromesso");
+				response = obj;
+				return response;
+			}
+			obj.put("Stato", stateCode);
+			JSONObject dato = setupModel(countryCode,stateCode);
+			long num = this.numEventi(dato);
+			obj.put("Numero Eventi", num);
+			response = obj;
+		}
+		
+		//MULTI STATE
+		if(states.length > 1) {
+			for (int i=0; i<states.length; i++) {
+				JSONObject singolo = new JSONObject();
+				//VALIDAZIONE STATE
+				boolean valStateCode = this.validazione("stateCode", states[i]);
+				if (!valStateCode) {
+					singolo.put("State", states[i]);
+					singolo.put("Errore", "stateCode non valido");
+					singolo.put("Messaggio", "Pattern di chiamata compromesso");
+				}
+				else {
+					JSONObject dato = setupModel(countryCode,states[i]);
+					long num = this.numEventi(dato);				
+					tot += num;
+					
+					//CALCOLO STATISTICHE
+					if(i==0) {
+						min = num;
+						indice_min = i;
+						max = num;
+						indice_max = i;	
+					}
+					else {
+						if(num<min) {
+							min = num;
+							indice_min = i;
+						}
+						if(num>max){
+							max = num;
+							indice_max = i;
+						}
+					}
+					
+					singolo.put("Numeri Eventi", num);
+					singolo.put("State", states[i]);
+				}
+				arr.add(singolo);
+			}
+			//GESTIONE STATISTICHE
+			JSONArray arr_stats = new JSONArray();
+			JSONObject stats = new JSONObject();
+			stats.put("Stato Min Eventi", states[indice_min] + "(" + min + ")"); 
+			stats.put("Stato Max Eventi", states[indice_max] + "(" + max + ")");
+			stats.put("Media Eventi Stato", tot/states.length); //POTREI CASTARLE A DOUBLE
+			arr_stats.add(stats);
+			
+			obj.put("Stati", arr);
+			obj.put("Stats", arr_stats);
+			
+			response = obj;
+		}
+		
+		return response;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public JSONObject getStats(String countryCode, String stateCode,String nameClass) throws ParseException {
+		//AGGIUNGERE PERIODO PER AVERE UNO STESSO METODO
+		JSONObject obj = new JSONObject();
+		JSONArray objs = new JSONArray();
+		JSONObject response = new JSONObject();
+		long num = 0;
+		String[] states = stateCode.split("-");
+		String[] classific = nameClass.split("-");
+		
+		//PER LE STATISTICHE
+		long tot = 0;
+		long min = 0;
+		int indice_min = 0;
+		long max = 0;
+		int indice_max = 0;
+		
+		//VALIDAZIONE PARAMETRO COUNTRYCODE
+		boolean valCountryCode = this.validazione("countryCode", countryCode);
+		if(!valCountryCode) {
+			obj.put("Country", countryCode);
+			obj.put("Errore", "countryCode non valido");
+			obj.put("Messaggio", "Pattern di chiamata compromesso");
+			response = obj;
+			return response;
+		}
+		
+		//AGGIUNGO IL DATO COUNTRY
+		obj.put("Country", countryCode);
+		
+		//SINGOLO STATO, SINGOLA CLASSIFICAZIONE
+		if (states.length == 1 && classific.length == 1) {
+			
+			//VALIDAZIONE DEI PARAMETRI CHE L'UTENTE IMMETTE
+			boolean valStateCode = this.validazione("stateCode", stateCode);
+			boolean valClass = this.validazione("segmentName", nameClass);
+			//SE UNO DEI PARAMETRI NON E' VALIDO MANDO UN ECCEZIONE
+			if (!valStateCode) {
+				obj.put("State", stateCode);
+				obj.put("Errore", "Stato non valido");
+				response = obj;
+				return response;
+			}
+			if (!valClass) {
+				obj.put("Classification", nameClass);
+				obj.put("Errore", "nameClass non valido");
+				response = obj;
+				return response;
+			}
+			
+			//ALTRIMENTI CONTINUO
+			num = this.numEventi(setupModel(countryCode,stateCode,nameClass));
+			obj.put("Country", countryCode);
+			obj.put("State", stateCode);
+			obj.put("Classification", nameClass);
+			obj.put("NUM", num);
+			response = obj;
+		}
+		
+		//SINGOLO STATO, MULTI CLASSIFICAZIONI
+		if(states.length == 1 && classific.length > 1) {
+			
+			//VALIDAZIONE DEI PARAMETRI CHE L'UTENTE IMMETTE
+			boolean valStateCode = this.validazione("stateCode", stateCode);
+			if(!valStateCode) {
+				obj.put("State", stateCode);
+				obj.put("Errore", "Stato non valido");
+				response = obj;
+				return response;
+			}
+			obj.put("State", stateCode);
+			for (int i=0; i<classific.length; i++) {
+				boolean valCla = this.validazione("segmentName", classific[i]);
+				JSONObject singola = new JSONObject();
+				if(!valCla) {
+					singola.put("Classificazione", classific[i]);
+					singola.put("Errore", "Classificazione non valida");
+				}
+				else {
+					num = this.numEventi(setupModel(countryCode,stateCode,classific[i]));
+					tot += num;
+					
+					//CALCOLO STATISTICHE
+					if(i==0) {
+						min = num;
+						indice_min = i;
+						max = num;
+						indice_max = i;	
+					}
+					else {
+						if(num<min) {
+							min = num;
+							indice_min = i;
+						}
+						if(num>max){
+							max = num;
+							indice_max = i;
+						}
+					}
+					
+					singola.put("Classificazione", classific[i]);
+					singola.put("NUM", num);
+				}
+				objs.add(singola);
+			}
+			
+			JSONArray arr_stats = new JSONArray();
+			JSONObject stats = new JSONObject();
+			stats.put("Genere Min Eventi", classific[indice_min] + "(" + min + ")");
+			stats.put("Genere Max Eventi", classific[indice_max] + "(" + max + ")");
+			stats.put("Media Eventi Cat", tot/classific.length);
+			arr_stats.add(stats);
+			
+			obj.put("Stats", arr_stats);
+			obj.put("Classificazioni", objs);
+			response = obj;
+		}
+		
+		//MULTI STATO, SINGOLA CLASSIFICAZIONE
+		if(states.length > 1 && classific.length == 1) {
+			//VALIDAZIONE CLASSIFICAZIONE
+			boolean valCla = this.validazione("segmentName", nameClass);
+			if(!valCla) {
+				obj.put("Classificazione", nameClass);
+				obj.put("Errore", "Classificazione non valida");
+				response = obj;
+				return response;
+			}
+			obj.put("Classificazione", nameClass);
+			for (int i=0; i<states.length; i++) {
+				boolean valSta = this.validazione("stateCode", states[i]);
+				JSONObject singola = new JSONObject();
+				if(!valSta) {
+					singola.put("Stato", states[i]);
+					singola.put("Errore", "Stato non valido");
+				}
+				else {
+					num = numEventi(setupModel(countryCode,states[i],nameClass));
+					tot += num;
+					
+					//CALCOLO STATISTICHE
+					if(i==0) {
+						min = num;
+						indice_min = i;
+						max = num;
+						indice_max = i;	
+					}
+					else {
+						if(num<min) {
+							min = num;
+							indice_min = i;
+						}
+						if(num>max){
+							max = num;
+							indice_max = i;
+						}
+					}
+					
+					
+					singola.put("Stato", states[i]);
+					singola.put("NUM", num);
+				}
+				objs.add(singola);
+			}
+			JSONArray arr_stats = new JSONArray();
+			JSONObject stats = new JSONObject();
+			stats.put("Stato Min Eventi", states[indice_min] + "(" + min + ")"); 
+			stats.put("Stato Max Eventi", states[indice_max] + "(" + max + ")");
+			stats.put("Media Eventi Stato", tot/states.length); //POTREI CASTARLE A DOUBLE
+			arr_stats.add(stats);
+			
+			obj.put("Stats", arr_stats);
+			obj.put("States", objs);
+			response = obj;
+		}
+		
+		//MULTI STATO, MULTI CLASSIFICAZIONE
+		if(states.length > 1 && classific.length > 1) {
+			//FACCIO L'ANALISI PER OGNI STATO
+			//RICHIAMO QUESTO STESSO METODO
+			for (String s : states) {
+				JSONObject singolo = this.getStats(countryCode, s, nameClass);
+				singolo.remove("Periodo"); //RIMUOVO LA PROPRIETA' PERIODO PER NON FARLA RIPETERE
+				objs.add(singolo);
+			}
+			obj.put("Stats", objs);
+			response = obj;
+		}
+		return response;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public JSONObject getStats(String countryCode, String stateCode, Periodo periodo) throws ParseException {
+		
+		JSONObject response = new JSONObject();
+		JSONObject obj = new JSONObject();
+		JSONArray arr= new JSONArray();
+		String[] states = stateCode.split("-");
+		
+		//PER LE STATISTICHE
+		long tot = 0;
+		long min = 0;
+		int indice_min = 0;
+		long max = 0;
+		int indice_max = 0;
+		
+		//VALIDAZIONE PARAMETRO COUNTRYCODE
+		boolean valCountryCode = this.validazione("countryCode", countryCode);
+		if(!valCountryCode) {
+			obj.put("Country", countryCode);
+			obj.put("Errore", "countryCode non valido");
+			obj.put("Messaggio", "Pattern di chiamata compromesso");
+			response = obj;
+			return response;
+		}
+		obj.put("Country", countryCode);
+		obj.put("Periodo", periodo);
+		
+		//SINGOLO STATE
+		if(states.length == 1) {
+			//VALIDAZIONE PARAMETRO STATECODE
+			boolean valStateCode = this.validazione("stateCode", stateCode);
+			if (!valStateCode) {
+				obj.put("State", stateCode);
+				obj.put("Errore", "stateCode non valido");
+				obj.put("Messaggio", "Pattern di chiamata compromesso");
+				response = obj;
+				return response;
+			}
+			obj.put("Stato", stateCode);
+			JSONObject dato = setupModel(countryCode,stateCode,periodo);
+			long num = this.numEventi(dato);
+			obj.put("Numero Eventi", num);
+			response = obj;
+		}
+		
+		//MULTI STATE
+		if(states.length > 1) {
+			for (int i=0; i<states.length; i++) {
+				JSONObject singolo = new JSONObject();
+				//VALIDAZIONE STATE
+				boolean valStateCode = this.validazione("stateCode", states[i]);
+				if (!valStateCode) {
+					singolo.put("State", states[i]);
+					singolo.put("Errore", "stateCode non valido");
+					singolo.put("Messaggio", "Pattern di chiamata compromesso");
+				}
+				else {
+					JSONObject dato = setupModel(countryCode,states[i],periodo);
+					long num = this.numEventi(dato);				
+					tot += num;
+					
+					//CALCOLO STATISTICHE
+					if(i==0) {
+						min = num;
+						indice_min = i;
+						max = num;
+						indice_max = i;	
+					}
+					else {
+						if(num<min) {
+							min = num;
+							indice_min = i;
+						}
+						if(num>max){
+							max = num;
+							indice_max = i;
+						}
+					}
+					
+					singolo.put("Numeri Eventi", num);
+					singolo.put("State", states[i]);
+				}
+				arr.add(singolo);
+			}
+			//GESTIONE STATISTICHE
+			JSONArray arr_stats = new JSONArray();
+			JSONObject stats = new JSONObject();
+			stats.put("Stato Min Eventi", states[indice_min] + "(" + min + ")"); 
+			stats.put("Stato Max Eventi", states[indice_max] + "(" + max + ")");
+			stats.put("Media Eventi Stato", tot/states.length); //POTREI CASTARLE A DOUBLE
+			arr_stats.add(stats);
+			
+			obj.put("Stati", arr);
+			obj.put("Stats", arr_stats);
+			
+			response = obj;
+		}
+		
+		return response;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public JSONObject getStats(String countryCode, String stateCode, String nameClass, Periodo periodo) throws ParseException {
+		
+		JSONObject obj = new JSONObject();
+		JSONArray objs = new JSONArray();
+		JSONObject response = new JSONObject();
+		long num = 0;
+		String[] states = stateCode.split("-");
+		String[] classific = nameClass.split("-");
+		
+		//PER LE STATISTICHE
+		long tot = 0;
+		long min = 0;
+		int indice_min = 0;
+		long max = 0;
+		int indice_max = 0;
+		
+		//VALIDAZIONE PARAMETRO COUNTRYCODE
+		boolean valCountryCode = this.validazione("countryCode", countryCode);
+		if(!valCountryCode) {
+			obj.put("Country", countryCode);
+			obj.put("Errore", "countryCode non valido");
+			response = obj;
+			return response;
+		}
+		
+		//AGGIUNGO IL DATO COUNTRY
+		obj.put("Country", countryCode);
+		
+		//SINGOLO STATO, SINGOLA CLASSIFICAZIONE --- NON HO LE STATS
+		if (states.length == 1 && classific.length == 1) {
+			
+			//VALIDAZIONE DEI PARAMETRI CHE L'UTENTE IMMETTE
+			boolean valStateCode = this.validazione("stateCode", stateCode);
+			boolean valClass = this.validazione("segmentName", nameClass);
+			//SE UNO DEI PARAMETRI NON E' VALIDO MANDO UN ECCEZIONE
+			if (!valStateCode) {
+				obj.put("State", stateCode);
+				obj.put("Errore", "Stato non valido");
+				response = obj;
+				return response;
+			}
+			if (!valClass) {
+				obj.put("Classification", nameClass);
+				obj.put("Errore", "nameClass non valido");
+				response = obj;
+				return response;
+			}
+			
+			//ALTRIMENTI CONTINUO
+			num = this.numEventi(setupModel(countryCode,stateCode,nameClass,periodo));
+			obj.put("Country", countryCode);
+			obj.put("State", stateCode);
+			obj.put("Classification", nameClass);
+			obj.put("NUM", num);
+			response = obj;
+			
+			//AGGIUNGO IL DATO PERIODO
+			response.put("Periodo", periodo);
+		}
+		
+		//SINGOLO STATO, MULTI CLASSIFICAZIONI --- HO LE STATS
+		if(states.length == 1 && classific.length > 1) {
+			
+			//VALIDAZIONE DEI PARAMETRI CHE L'UTENTE IMMETTE
+			boolean valStateCode = this.validazione("stateCode", stateCode);
+			if(!valStateCode) {
+				obj.put("State", stateCode);
+				obj.put("Errore", "Stato non valido");
+				response = obj;
+				return response;
+			}
+			obj.put("State", stateCode);
+			for (int i=0; i<classific.length; i++) {
+				boolean valCla = this.validazione("segmentName", classific[i]);
+				JSONObject singola = new JSONObject();
+				if(!valCla) {
+					singola.put("Classificazione", classific[i]);
+					singola.put("Errore", "Classificazione non valida");
+				}
+				else {
+					num = this.numEventi(setupModel(countryCode,stateCode,classific[i],periodo));
+					tot += num;
+					
+					//CALCOLO STATISTICHE
+					if(i==0) {
+						min = num;
+						indice_min = i;
+						max = num;
+						indice_max = i;	
+					}
+					else {
+						if(num<min) {
+							min = num;
+							indice_min = i;
+						}
+						if(num>max){
+							max = num;
+							indice_max = i;
+						}
+					}
+					
+					singola.put("Nome", classific[i]);
+					singola.put("Numero Eventi", num);
+				}
+				objs.add(singola);
+			}
+			
+			JSONArray arr_stats = new JSONArray();
+			JSONObject stats = new JSONObject();
+			stats.put("Genere Min Eventi", classific[indice_min] + "(" + min + ")");
+			stats.put("Genere Max Eventi", classific[indice_max] + "(" + max + ")");
+			stats.put("Media Eventi Cat", tot/classific.length);
+			arr_stats.add(stats);
+			
+			obj.put("Classificazioni", objs);
+			obj.put("Tot Eventi", tot);
+			obj.put("Stats", arr_stats);
+			response = obj;
+			
+			//AGGIUNGO IL DATO PERIODO
+			response.put("Periodo", periodo);
+		}
+		
+		//MULTI STATO, SINGOLA CLASSIFICAZIONE --- HO LE STATS
+		if(states.length > 1 && classific.length == 1) {
+			//VALIDAZIONE CLASSIFICAZIONE
+			boolean valCla = this.validazione("segmentName", nameClass);
+			if(!valCla) {
+				obj.put("Classificazione", nameClass);
+				obj.put("Errore", "Classificazione non valida");
+				response = obj;
+				return response;
+			}
+			//SE LA VALIDAZIONE E' ANDATA A BUON FINE
+			obj.put("Classificazione", nameClass);
+			
+			for (int i=0; i<states.length; i++) {
+				boolean valSta = this.validazione("stateCode", states[i]);
+				JSONObject singola = new JSONObject();
+				if(!valSta) {
+					singola.put("Stato", states[i]);
+					singola.put("Errore", "Stato non valido");
+				}
+				else {
+					num = this.numEventi(setupModel(countryCode,states[i],nameClass,periodo));
+					tot += num;
+					
+					//CALCOLO STATISTICHE
+					if(i==0) {
+						min = num;
+						indice_min = i;
+						max = num;
+						indice_max = i;	
+					}
+					else {
+						if(num<min) {
+							min = num;
+							indice_min = i;
+						}
+						if(num>max){
+							max = num;
+							indice_max = i;
+						}
+					}
+
+					singola.put("Stato", states[i]);
+					singola.put("Numero Eventi", num);
+				}
+				objs.add(singola);
+			}
+			
+			JSONArray arr_stats = new JSONArray();
+			JSONObject stats = new JSONObject();
+			stats.put("Stato Min Eventi", states[indice_min] + "(" + min + ")"); 
+			stats.put("Stato Max Eventi", states[indice_max] + "(" + max + ")");
+			stats.put("Media Eventi Stato", tot/states.length); //POTREI CASTARLE A DOUBLE
+			arr_stats.add(stats);
+			
+			obj.put("States", objs); //AGGIUNGO I NUMERO DEGLI EVENTI
+			obj.put("Stats", arr_stats); //AGGIUNGO LE STATS ALL'OGGETTO DI RISPOSTA
+			response = obj;
+			
+			//AGGIUNGO IL DATO PERIODO
+			response.put("Periodo", periodo);
+		}
+		
+		//MULTI STATO, MULTI CLASSIFICAZIONE
+		if(states.length > 1 && classific.length > 1) {
+			//FACCIO L'ANALISI PER OGNI STATO
+			//RICHIAMO QUESTO STESSO METODO
+			for (String s : states) {
+				JSONObject singolo = this.getStats(countryCode, s, nameClass, periodo);
+				singolo.remove("Periodo"); //RIMUOVO LA PROPRIETA' PERIODO PER NON FARLA RIPETERE
+				//singolo.remove("Stats"); //POTREI RIMUOVERE LE STATS PER FARNE ALTRE
+				objs.add(singolo);
+			}
+			obj.put("States", objs);
+			response = obj;
+			
+			//AGGIUNGO IL DATO PERIODO
+			response.put("Periodo", periodo);
+		}
 		return response;
 	}
 	
